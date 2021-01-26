0579bb7190d9c895cc049ca83fbd2051b57c502b
Veronica Pak <nikampak@gmail.com>
2021-01-26 15:16:42 -0500

    added a new branch feature-d: updated Hello file

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -3,5 +3,7 @@ public class Hello {
         System.out.println("Hello World");
         // more work
         // working working for the third commit
+        System.out.println("Changes to the first file while opening the new" +
+                " branch, feature-d");
     }
 }
